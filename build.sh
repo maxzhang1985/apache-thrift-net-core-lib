bfb25454329531a65e095fab2518c3043fb3fa48
Volodymyr Gotra <vgotra@gmail.com>
2016-09-03 21:52:29 +0300

    Updated build configuration

--- build.sh
+++ build.sh
@@ -3,16 +3,16 @@
 #exit if any command fails
 set -e
 
-dotnet --verbose --info
+dotnet --info > dotnetbuild.log
 
-dotnet --verbose restore **/*/project.json
+dotnet --verbose restore **/*/project.json > dotnetbuild.log
 
 # dotnet test ./test/TEST_PROJECT_NAME -c Release -f netcoreapp1.0
 
 # Instead, run directly with mono for the full .net version 
-dotnet --verbose build **/*/project.json -r win10-x64
-dotnet --verbose build **/*/project.json -r osx.10.11-x64
-dotnet --verbose build **/*/project.json -r ubuntu.16.04-x64
+dotnet --verbose build **/*/project.json -r win10-x64 > dotnetbuild.log
+dotnet --verbose build **/*/project.json -r osx.10.11-x64 > dotnetbuild.log
+dotnet --verbose build **/*/project.json -r ubuntu.16.04-x64 > dotnetbuild.log
 
 #revision=${TRAVIS_JOB_ID:=1}  
 #revision=$(printf "%04d" $revision) 
